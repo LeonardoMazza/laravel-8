b9a2bb143af66ea11028af237cf8597e96d9dda4
Leonardo Mazza <desenvolvedormazza@gmail.com>
2021-06-18 18:54:24 -0300

    Admin LTE

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,21 @@ use App\Http\Controllers\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::put('/posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
 
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::middleware(['auth'])->group(function () {
+    Route::put('/posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
 
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
